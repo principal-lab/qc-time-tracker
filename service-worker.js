9000b962acfb6da8127f1e27b4c3e7cb45f1f994
principal-lab <principal@qcaviation.com.au>
2025-11-10 12:53:32 +1100

    Add files via upload
    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'qc-time-tracker-v1';
+const urlsToCache = [
+  './time_tracker_pwa.html',
+  './QC_Logo_Letterhead_241111.jpg',
+  './manifest.json'
+];
+
+// Install service worker and cache resources
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache first, then network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update service worker and clear old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
